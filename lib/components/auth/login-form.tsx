f5d1c1d4b43994efea42094ca78aa9f4517381d3
Reece <reecemorgandev@gmail.com>
2024-07-01 15:38:42 +0100

    successful log in!

--- lib/components/auth/login-form.tsx
+++ lib/components/auth/login-form.tsx
@@ -7,16 +7,26 @@ export const LoginForm = () => {
   const router = useRouter();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [showError, setShowError] = useState<boolean>(false);
+  const [errorMsg, setErrorMsg] = useState<string>("");
 
   const submit = async (e: SyntheticEvent) => {
     e.preventDefault();
 
-    await fetch("http://localhost:5189/api/login", {
+    const data = await fetch("http://localhost:5189/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       credentials: "include",
       body: JSON.stringify({ email, password }),
     });
+
+    const res = await data.json();
+
+    if (res.message === "Invalid Credentials") {
+      setErrorMsg("Incorrect email or password. Please try again.");
+      setShowError(true);
+      return;
+    }
 
     await router.push("/");
   };
@@ -37,6 +47,7 @@ export const LoginForm = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
       <button type="submit">Sign In</button>
+      {showError && <p>{errorMsg}</p>}
     </form>
   );
 };
